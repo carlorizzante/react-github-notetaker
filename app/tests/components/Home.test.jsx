4f9d79517635d5dbd444edf557fb2fda18694a34
Carlo Rizzante <hello@carlorizzante.com>
2016-07-13 12:29:48 +0200

    Adds initial tests

---
+++ app/tests/components/Home.test.jsx
@@ -0,0 +1,10 @@
+var expect = require("expect");
+var React = require("react");
+
+var Home = require("Home");
+
+describe("Home", () => {
+  it("should exist", () => {
+    expect("Home").toExist();
+  });
+});
---
+++ app/tests/components/Main.test.jsx
@@ -0,0 +1,10 @@
+var expect = require("expect");
+var React = require("react");
+
+var Main = require("Main");
+
+describe("Main", () => {
+  it("should exist", () => {
+    expect("Main").toExist();
+  });
+});
