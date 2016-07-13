fce1e9c411d54d5a5ca8fb4a4c3a87bc6822afaf
Carlo Rizzante <hello@carlorizzante.com>
2016-07-13 14:34:39 +0200

    Adds NotesList component and basic testing

---
+++ app/tests/components/Notes/NotesList.test.jsx
@@ -0,0 +1,10 @@
+var expect = require("expect");
+var React = require("react");
+
+var NotesList = require("NotesList");
+
+describe("NotesList", () => {
+  it("should exist", () => {
+    expect("NotesList").toExist();
+  });
+});
